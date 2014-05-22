2052f472ad79729ecb078cde385497b0616e86ae
Paresh Gupta <paresh@vinsol.com>
2014-05-22 21:05:01 +0530

    chaining done

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -5,21 +5,23 @@ $(document).ready(function() {
   });
 
 // Select the search input field & traverse up to form & add a class to it.
-  var searchField = $("input[name = 'q']").css('background-color', 'yellow');
-  searchField.parent().addClass('searchForm');
+  $("input[name = 'q']").css('background-color', 'yellow')
+                        .parent()
+                        .addClass('searchForm');
 
 // Select the list item having class "current" inside #myList and remove that class, adding a class "current" to the next list item
-  var listItem = $("#myList li.current").css("background-color", "red");
-  listItem.removeClass("current");
-  listItem.next().addClass("current");
+  $("#myList li.current").css("background-color", "red")
+                          .removeClass("current")
+                          .next()
+                          .addClass("current");
 
 // Select the select element inside #specials then traverse to the submit button.
-  var select = $("#specials").find("select").css("background-color", "green");
-  select.parent().next().children().css("border", "2px solid green");
+  var firstList = $("#specials li:first");
+  firstList.children().css("background-color", "green");
+  firstList.next().children().css("background-color", "green");
 
 // Select the first list item in the #slideshow element adding class "current" to it, and then add a class of "disabled" to its sibling elements
-  var list = $("#slideshow li").first().addClass('current');
-  list.children().each(function() {
+  $("#slideshow li:first").addClass('current').children().each(function() {
     $(this).addClass("disabled");
   });
 });
