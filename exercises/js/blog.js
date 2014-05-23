7476aaf5457ac9fee1801bbab39cd4467046963e
Paresh Gupta <paresh@vinsol.com>
2014-05-23 18:19:58 +0530

    exercise6 added

---
+++ exercises/js/blog.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+// Clicking on headline should slide down its excerpt paragraph, and slide up any other currently showing excerpt paragraphs.
+  $("#blog h3").each(function() {
+    $(this).click(function(event) {
+      event.preventDefault();
+      $(this).siblings().show();
+      $(this).parent().siblings().children(".excerpt").hide();
+    });
+  });
+});
