7868a98d523fb1abab2642764b74a9e07ad0a31d
Paresh Gupta <paresh@vinsol.com>
2014-05-24 12:25:52 +0530

    navigation exercise added

---
+++ exercises/js/tabs.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+// hide all elements having class "module"
+  $(".module").hide();
+
+//creating unordered list element
+  var $newUnorderedList = $("<ul>").attr("id", "moduleList")
+                                   .insertBefore(".module:first");
+
+// itereate to class "module" elements and adding new List item to the unordered list
+  $(".module h2").each(function() {
+    var $newList = $("<li>").addClass($(this).parent().attr("id"))
+                            .text($(this).text())
+                            .appendTo($newUnorderedList);
+  });
+
+// Bind a click event to the list item that:
+  // Shows the related module, and hides any other modules
+  // Adds a class of "current" to the clicked list item
+  // Removes the class "current" from the other list item
+  $("#moduleList li").on("click", function() {
+    var $elementId = "#" + $(this).attr("class");
+    $($elementId).show().siblings(".module").hide();
+    $(this).addClass("current").siblings().removeClass("current");
+  });
+
+// show first tab
+  $(".module:first").show();
+});
