544bb868592af8237d408fc0ab45c1ac493cd03d
Paresh Gupta <paresh@vinsol.com>
2014-05-26 12:02:57 +0530

    slide method included

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 // Clicking on headline should slide down its excerpt paragraph, and slide up any other currently showing excerpt paragraphs.
   $("#blog h3").click(function(event) {
     event.preventDefault();
-    $(this).siblings().show();
-    $(this).parent().siblings().children(".excerpt").hide();
+    $(this).siblings().slideDown(1000);
+    $(this).parent().siblings().children(".excerpt").slideUp(1000);
   });
 });
