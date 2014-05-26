e47b4d8189514c63be73588cf092774f63def3a6
Paresh Gupta <paresh@vinsol.com>
2014-05-26 18:38:08 +0530

    code refactored

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,29 +1,40 @@
-// function to slideshow the images
-function slideshow(length) {
-  $count++;
-  var navigateImageDetail = "<<< " + $count + " / " + length + " >>>";
-  $newNavigateElement.text(navigateImageDetail);
-  $slideshowElement.fadeIn(1000).delay(1500).fadeOut(function() {
-    if($count !== length) {
-      $slideshowElement = $slideshowElement.next();
+function Slideshow(slides, element) {
+  this.count = 0;
+  this.slides = slides;
+  this.slideText = element;
+
+  // method to slide images
+  this.slideshow = function(length) {
+    var that = this;
+    this.count++;
+    this.slideCount = "<<< " + this.count + " / " + length + " >>>";
+    this.slideText.text(this.slideCount);
+    this.slides.fadeIn(1000)
+               .delay(1500)
+               .fadeOut(function() { that.nextSlide(length) });
+  }
+
+  // method to check the no of slide and move to next slide
+  this.nextSlide = function(length) {
+    if(this.count !== length) {
+      this.slides = this.slides.next();
     } else {
-      $slideshowElement = $("#slideshow li").first();
-      $count = 0;
+      this.slides = $("#slideshow li:first");
+      this.count = 0;
     }
-    slideshow(length);
-  });
+    this.slideshow(length);
+  }
 }
 
-$(document).ready(function() {
-  $slideshowElement = $("#slideshow li").first();
-  $slideshowElementLength = $("#slideshow li").hide().length;
-  $count = 0;
-  // creating navigation image area below slideshow
-  $newNavigateElement = $("<p>").addClass("current").appendTo($("#slideshow"));
-  
-  // prepend the #slideshow elements to top of the page
-  $("body").prepend($("#slideshow"));
+$(function() {
+  var $slides = $("#slideshow li:first");
+  var $slideLength = $("#slideshow li").hide().length;
+  // creating a element for slide counts
+  var $slideTextElement = $("<p>").addClass("current").appendTo("#slideshow");
+  // prepending all images to the body 
+  $("#slideshow").prependTo("body");
 
-  // calling a function slideshow
-  slideshow($slideshowElementLength);
+  // new instance of the Slideshow function
+  var imageSlides = new Slideshow($slides, $slideTextElement);
+  imageSlides.slideshow($slideLength);    
 });
