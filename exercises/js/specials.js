36b2f82753434c81ab1e1bde226cc11e4e4c8e23
Paresh Gupta <paresh@vinsol.com>
2014-05-27 14:54:40 +0530

    exercise 10 added

--- exercises/js/specials.js
+++ exercises/js/specials.js
@@ -1,14 +1,41 @@
+// function to load the JSON file 
+function getJSON() {
+  var loadData = '';
+  $.ajax({ url : 'data/specials.json',
+    dataType : 'json',
+    async : false,
+    success : function(data) {
+      loadData = data;
+    }
+  })
+  .done(function() { console.log( "Successfully Loaded" ); })
+  .fail(function() { console.log("Error on Loading"); });
+  return loadData;
+}
+
 $(document).ready(function() {
+  var $dataJSON = '';
   // Appending div element after form in "#special"
-  var $newDivElement = $('<div>').attr('id', 'newDiv');
-  $('#specials form[action = "specials.html"]').after($newDivElement);
+  var $newDivElement = $('<div>').attr('id', 'targetDiv');
+  $('#specials form').after($newDivElement);
+
+  // remove submit button in the form
+  $("input[type = 'submit']").parent().remove();
 
   // Bind select elements to change event and load Ajax request
-  var $targetURL = "data/" + $('#specials form').attr('action').replace('html', 'json');
   $("select[name = 'day']").bind('change', function() {
-    $.getJSON($targetURL, function(data) {
-      $newDivElement.text(data.monday);
-    })
-    
+    var $selectedOption = $(this).val();
+    if($dataJSON.length === 0) {
+      $dataJSON = getJSON();
+    }
+
+    if($selectedOption) {
+      $('#targetDiv').html($dataJSON[$selectedOption].title + '<br/>' + $dataJSON[$selectedOption].text);
+    } else {
+      $('#targetDiv').text('');
+    }
   });
 });
+  
+
+
