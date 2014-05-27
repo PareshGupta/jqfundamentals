99c4e897c20fa55441adf9a8ed1b1509ca118d89
Paresh Gupta <paresh@vinsol.com>
2014-05-28 01:12:20 +0530

    code refactored

--- exercises/exercise11/exercise11.js
+++ exercises/exercise11/exercise11.js
@@ -1,21 +1,40 @@
-$(document).ready(function() {
-  // creating button to add div's
-  var $addButton = $('<button>').attr('id', 'addItem').text('Add Item');
-  $('body').prepend($addButton);
+function Stack() {
+  this.setup = function() {
+    this.addButton();
+    this.addDiv();
+    this.highlightDiv();
+    this.removeFirstDiv();
+  }
 
-  // adding stack of divs on clicking the button
-  $('#addItem').bind('click', function() {
-    var $divCount = $("#main_container").children().length + 1;
-    var $divStack = $("<div>").addClass("sub_containers").text("Div No. " + $divCount);
+  this.addButton = function() {
+    $('<button />').attr('id', 'addItem').text('Add Item').prependTo('body');
+  }
+
+  this.addDiv = function() {
+    var that = this;
+    $('#addItem').click(function() { that.createDiv() } );
+  }
+
+  this.createDiv = function() {
+    var divCount = $("#main_container").children().length + 1;
+    var $divStack = $("<div>").addClass("sub_containers").text("Div No. " + divCount);
     $divStack.prependTo("#main_container");
-  });
-  
-  // highlighting the div's by binding event
-  $('#main_container').on('click', '.sub_containers', function() {
-    $(this).toggleClass('highlight');
-  });
+  }
 
-  $('#main_container').on('click', '.sub_containers:first', function() {
-    $(this).remove();
-  });
+  this.highlightDiv = function() {
+    $('#main_container').on('click', '.sub_containers', function() {
+      $(this).toggleClass('highlight');
+    });
+  }
+
+  this.removeFirstDiv = function() {
+    $('#main_container').on('click', '.sub_containers:first', function() {
+      $(this).remove();
+    });
+  }
+}
+
+$(function() {
+  var stack = new Stack();
+  stack.setup();
 });
