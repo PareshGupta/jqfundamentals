070e4974e8e21b05b0b91fd8843414ffa4025bb8
Paresh Gupta <paresh@vinsol.com>
2014-05-27 18:39:58 +0530

    variable name change

--- exercises/js/inputHint.js
+++ exercises/js/inputHint.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 // removing the label element and savinn it's text 
-  var $hintText = $("label[for = 'q']").remove().text();
+  var hintText = $("label[for = 'q']").remove().text();
 
 // setting the value of search input to the label text and adding class "hint"
-  $("input[name = 'q']").val($hintText).addClass("hint");
+  $("input[name = 'q']").val(hintText).addClass("hint");
 
 // bind focus event to search input that removes the hint text & 'hint' class
   $('.input_text').focus(function() {
     if($(this).hasClass('hint')) {
-      $(this).val("").removeClass("hint");
+      $(this).val("").removeClass('hint');
     }
   });
 
 // bind blur event to search input restoring the hint text and class "hint"
   $('.input_text').blur(function() {
     if(!$(this).val()) {
-      $(this).val($hintText).addClass("hint");
+      $(this).val(hintText).addClass('hint');
     } 
   });
 });
