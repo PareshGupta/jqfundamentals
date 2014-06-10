363ccbf5d30b2df0ac2557546e4cb76577c4d846
paresh <paresh@vinsol.com>
2014-06-11 02:01:30 +0530

    row class added

--- exercises/EmployeeRole/employeeRole.js
+++ exercises/EmployeeRole/employeeRole.js
@@ -12,33 +12,76 @@ var App = {
 
 	setup : function() {
 		this.addEmployees();
+		this.addRoles();
 	},
 
 	addEmployees : function() {
 		$(this.seedData["employees"]).each(function(index, name) {
 			var employee = new Employee(name);
 		});
+	},
+
+	addRoles : function() {
+		$(this.seedData["roles"]).each(function(index, role) {
+			var role = new Role(role);
+		});
 	}
 
 }
 
+// Employee Class
 function Employee(name) {
 	this.name = name;
+	this.init();
+}
+
+Employee.prototype.init = function() {
 	this.add();
-	this.draggable();
+	this.draggable();	
 }
 
 Employee.prototype.add = function() {
-	$("<li>", { text : this.name } ).appendTo($("#employees"));
+	$("<li>", { text : this.name }).appendTo($("#employees"));
 }
 
 Employee.prototype.draggable = function() {
 	$("#employees li").draggable({
 		snap : true,
 		cursor : "move",
-		helper : "clone"
+		helper : "clone",
+		stop: function(event, ui) {
+      // $(li.helper).clone(true);						// issue 1 : drag only copy of the employee name
+    }
 	}).disableSelection();
 }
+
+// Role Class
+function Role(role) {
+	this.role = role;
+	this.init();
+}
+
+Role.prototype.init = function() {
+	this.add();
+	this.droppable();		
+}
+
+Role.prototype.add = function() {
+	$("<ul>").data("role", this.role).html($("<li>", { text : this.role })).appendTo("#roles");
+}
+
+Role.prototype.droppable = function() {
+	$("#roles ul").droppable({
+		drop : function(event, ui) {
+			if($(this).find(ui).length) {
+				return 0;
+			} else {
+				ui.draggable.appendTo($(this));		
+			}
+		}
+	});
+}
+
 
 $(function() {
 	App.init();
